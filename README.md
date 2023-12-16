800a1d41d45aa7c97299b0ed78c2039aca446d01
Will Thong <will@willthong.com>
2023-12-16 19:12:13 +0000

    Replace manual parsing with ics package

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # Binfluencer 🗑🚮📆
 
-A Python application to fetch the bin collection dates from your UK local council's website.
-It will then let you know if you have to put a bin out today. OF course, you could
-manually download the council-provided ICS file and add it to your calendar, but then
-you need to add notifications for the day before, or remember to check your calendar,
-and (worst of all), you need to go through this task every time the council adds new bin
-dates. For my council, that's every three months, and I forget.
+A Python application to fetch the bin collection dates from your UK local council's
+website. It will then let you know if you have to put a bin out today.
 
-With Binfluencer, you will never forget the bins again! Binfluencer can also be run on
+Of course, you could subscribe to the calendar your council provides, but there are two
+problems with this approach. First, if you use Google Calendar, sometimes that fails to
+sync ([this might
+help](https://www.ryadel.com/en/google-calendar-force-update-refresh-subscribed-calendar-ics/)
+if you're experiencing this problem). And, second, most people put their bins out the
+night before collection. Google Calendar does not allow us to create an event which is
+always the day before another event, only events on a recurring basis (which breaks down
+if there is a public holiday). So you can either remember to check our phone for events
+the next day, or Binfluencer can remind you.
+
+With Binfluencer, you will never forget the bins again! Binfluencer can be run on
 your [Home Assistant](https://home-assistant.io) server as a script so you can (for
 instance) have a smart speaker nag you from the second you get home on bin night until
 you finally put that bin out.
@@ -21,11 +27,11 @@ others don't. I am pretty sure those that do use the same software contractor du
 similarities though. Here's an incomplete list of councils I've manually checked for
 compatibility:
 
-❌ Adur & Worthing Councils\\
-❌ Barnet Council\\
-✅ Cambridge City Council\\
-❌ Islington Council\\
-✅ South Cambridgeshire District Council\\
+❌ Adur & Worthing Councils  
+❌ Barnet Council  
+✅ Cambridge City Council  
+❌ Islington Council  
+✅ South Cambridgeshire District Council  
 
 ## Installation ⚙️
 
@@ -44,8 +50,19 @@ These settings can be changed in config.json:
   *Default: the project directory*
 * `refresh_rate`: How often (integer value of days from 1 to 365) should Binfluencer
   update your calendar? *Default: 90*
+* `colours`: Which colour of bins do you want to track? *Default: ["black", "blue", "green"]*
 
 ## Usage 🗑️
+
+```
+# Returns which bins you have to put out next, and when: 'Tomorrow (Blue and Green)'
+python binfluencer.py 
+
+# Returns 'Next blue bin collection: 1 February 2024; Next green bin collection: 
+# Blue and Green / Next black bin collection: 8 February 2024 (Black)'
+python binfluencer.py --verbose
+
+```
 
 ## Roadmap 🗺️
 
--- binfluencer.py
+++ binfluencer.py
@@ -1,25 +1,32 @@
 import argparse
+import arrow
 import datetime as dt
+import ics
 import json
 import logging
 import os
-from pathlib import Path
+import pytz
 import requests
 import sys
 import validators
+import zoneinfo
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.WARNING,
     filename=script_dir + "/binfluencer.log",
     filemode="w",
     format=f"%(asctime)s - %(levelname)s - %(message)s",
 )
 logging.getLogger().addHandler(logging.StreamHandler())
 
+class BinCollection():
+    def __init__(self, colour: str, date: dt.datetime=None) -> None:
+        self.colour = colour
+        self.date = date
 
-def get_valid_url() -> str:
+def new_config_url() -> str:
     """Accepts user input, strips and returns it if it's a valid URL"""
     url_prompt = (
         "Enter your property's bin calendar URL here. If you're not sure "
@@ -32,23 +39,78 @@ def get_valid_url() -> str:
         if not validators.url(user_input):
             logging.error(f"{user_input} is an invalid URL. Please enter a valid URL.")
         else:
-            return user_input
+            config["url"] = user_input
+            config_json = json.dumps(config, indent=4)
+            with open(script_dir + "/config.json", "w") as config_file:
+                config_file.write(config_json)
+            logging.debug(f"Saved new URL {config['url']} to config.json")
+            return
 
 
-def create_event(event_date, event_name):
-    event = {"date": event_date, "name": event_name}
-    events.append(event)
+def parse_ics(ics_file: str, next_collections: list[BinCollection]) -> list[BinCollection]:
+    """Downloads ICS file and adds each bin colour's latest date to the next_collections list"""
+    with open(ics_file, mode="r") as file:
+        cal = ics.Calendar(file.read())
+        now = dt.datetime.now().replace(tzinfo=zoneinfo.ZoneInfo("Europe/London"))
+
+        for event in cal.events:
+            if event.begin.datetime <= now:
+                continue
+            for bc in next_collections:
+                if not bc.colour in event.name.lower():
+                    continue
+                logging.debug(f"Found {bc.colour} bin collection on {event.begin.datetime.strftime('%Y-%m-%d')}")
+                if not bc.date:
+                    bc.date = event.begin.datetime
+                elif bc.date:
+                    bc.date = min(bc.date, event.begin.datetime)
+                break
+        logging.debug(
+            "Found next bin collection dates: "
+            f"{[[bc.date.strftime('%Y-%m-%d'), bc.colour] for bc in next_collections]}"
+        )
+        return next_collections
 
 
-def get_colour(event_name):
-    return event_name.split()[0].lower()
+def consolidate_bins(next_collections: list[BinCollection]) -> list[BinCollection]:
+    """Combines any BinCollections on the same date"""
+    next_collections.sort(key=lambda x: x.date)
+    new_next_collections = [next_collections[-1]]
+    i = 0
+    while i < len(next_collections) - 1:
+        bc = next_collections[i]
+        next_bc = next_collections[i+1]
+        if bc.date == next_bc.date:
+            new_next_collections.append(BinCollection(
+                bc.colour.capitalize() + " and " + next_bc.colour.capitalize(), bc.date
+            ))
+            logging.debug(f"Consolidated {bc.colour} and {next_bc.colour}")
+            i += 2
+        else:
+            bc.colour = bc.colour.capitalize()
+            new_next_collections.append(bc)
+            i += 1
+    return new_next_collections
 
 
-def find_next(colour, events):
-    for event in events:
-        event_colour = get_colour(event["name"])
-        if event_colour == colour:
-            return event["date"]
+def next_bin(next_collections: list[BinCollection]) -> str:
+    for bc in next_collections:
+        if dt.datetime.now().date() == bc.date.date():
+            return f"Today ({bc.colour})"
+        elif dt.datetime.now().date() + dt.timedelta(days=1) >= bc.date.date():
+            return f"Tomrorrow ({bc.colour})"
+        elif dt.datetime.now().date() + dt.timedelta(days=7) >= bc.date.date():
+            return f"{bc.date.strftime('%A')} ({bc.colour})"
+
+
+def next_bin_verbose(next_dates: dict) -> str:
+    next_collections.sort(key=lambda x: x.date)
+    verbose_string = ""
+    for bc in next_collections:
+        if "and" in bc.colour:
+            bc_plural = "s"
+        verbose_string += f"\nNext {bc.colour.lower()} bin{bc_plural}: {bc.date.strftime('%d %b %Y')}"
+    return verbose_string
 
 
 try:
@@ -62,26 +124,24 @@ except FileNotFoundError:
 # TODO: CLI interface for customising config
 
 if "url" not in config:
-    config["url"] = get_valid_url()
-    config_json = json.dumps(config, indent=4)
-    with open(script_dir + "/config.json", "w") as config_file:
-        config_file.write(config_json)
-    logging.debug(f"Saved new URL {config['url']} to config.json")
+    new_config_url()
     # TODO: delete any existing ICS files
 else:
     logging.debug(f"Loaded URL {config['url']} from config.json")
 
-# TODO: (config["ics_location"]) customisation
+# TODO: Customise config["ics_location"]
 ics_file = config["ics_location"] if "ics_location" in config else script_dir
 ics_file += "/bin_dates.ics"
 
 refresh_rate = config["refresh_rate"] if "refresh_rate" in config else 90
 
-ics_mod = dt.datetime.fromtimestamp(os.path.getmtime(ics_file))
-since_refresh = (dt.datetime.now() - ics_mod).days
-logging.info(f"{since_refresh} days since the bin collection dates were last updated.")
-if since_refresh >= refresh_rate:
-    logging.info(f"Refreshing...")
+since_refresh = None
+if os.path.exists(ics_file):
+    ics_mod = dt.datetime.fromtimestamp(os.path.getmtime(ics_file))
+    since_refresh = (dt.datetime.now() - ics_mod).days
+    logging.info(f"{since_refresh} days since bin collection dates were last updated.")
+if since_refresh is None or since_refresh >= refresh_rate:
+    logging.info(f"Downloading bin collection file...")
     r = requests.get(config["url"])
     with open(ics_file, "wb") as file:
         for chunk in r.iter_content(chunk_size=128):
@@ -89,75 +149,20 @@ if since_refresh >= refresh_rate:
 else:
     logging.info(f"Skipping file refresh.")
 
-events, event_date, event_name, looking_for_event = [], "", "", False
-
-with open(ics_file, mode="r", encoding="utf-8") as file:
-    line_index = 0
-    for line in file:
-        if line.startswith("BEGIN:VEVENT"):
-            looking_for_event = True
-        if line.startswith("END:VEVENT"):
-            looking_for_event = False
-            create_event(event_date, event_name)
-        if looking_for_event and line.startswith("DTSTART;VALUE=DATE:"):
-            event_date = int((line.split(":")[1]).strip())
-        if looking_for_event and line.startswith("SUMMARY:"):
-            event_name = (line.split(":")[1]).strip()
-        line_index += 1
-
-# Remove dates in the past
-events_future = []
-today = dt.date.today()
-today = int(today.strftime("%Y%m%d"))
-for event in events:
-    if event["date"] >= today:
-        events_future.append(event)
-
-# Sort list of events by date
-dates = []
-last_date = 0
-for event in events_future:
-    dates.append(event)
-    if event["date"] > last_date:
-        dates.remove(event)
-        dates.append(event)
-    last_date = event["date"]
-next_bin_date = dates[0]["date"]
-next_bin_date_str = str(dates[0]["date"])
-next_bin_date_str = (
-    next_bin_date_str[0:4] + "-" + next_bin_date_str[4:6] + "-" + next_bin_date_str[6:8]
-)
-
-
-next_black = find_next("black", events_future)
-next_blue = find_next("blue", events_future)
-next_green = find_next("green", events_future)
-
-next_bin_colours = []
-for event in events_future:
-    if event["date"] == next_bin_date:
-        next_bin_colours.append(get_colour(event["name"]))
-if "blue" in next_bin_colours and "green" in next_bin_colours:
-    nbc_output = "Blue and Green"
+if "colours" not in config:
+    next_collections = [BinCollection("black"), BinCollection("blue"), BinCollection("green")]
 else:
-    nbc_output = next_bin_colours[0].title()
+    next_collections = [BinCollection(colour.lower()) for colour in config["colours"]]
+
+next_collections = parse_ics(ics_file, next_collections)
+next_collections = consolidate_bins(next_collections)
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--date",
-    action=argparse.BooleanOptionalAction,
-    help="Show next bin collection date",
-)
-parser.add_argument(
-    "--colour",
-    action=argparse.BooleanOptionalAction,
-    help="Show next bin collection colour",
-)
+parser.add_argument( "--verbose", action=argparse.BooleanOptionalAction)
 args = parser.parse_args()
-if args.date:
-    sys.stdout.write(f"{next_bin_date_str}")
-if args.colour:
-    sys.stdout.write(f"{nbc_output}")
+if args.verbose:
+    sys.stdout.write(next_bin_verbose(next_collections))
+else:
+    sys.stdout.write(next_bin(next_collections))
 
 # TODO: add tests
-# TODO: weird edge case if it's today
